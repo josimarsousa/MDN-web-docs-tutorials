be0f515b94d1211518b738e1ee81df02e330cbf5
Josimar <josimar@Mac-mini-de-Josimar.local>
2020-06-16 07:00:14 -0300

    udpdate index.html added main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1')
-myHeading.textContent = 'Hello World !' 
+let myImage = document.querySelector('img')
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src')
+    if(mySrc === 'images/firefox.jpg'){
+        myImage.setAttribute('src', 'images/fox.jpg')
+    }else{
+        myImage.setAttribute('src', 'images/firefox.jpg')
+    }
+}
